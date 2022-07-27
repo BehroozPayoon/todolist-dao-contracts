2115f5a45434c8e25ab8f4d2dd11658d0aff1131
Behrooz Payoon <Behrooz.payoon@gmail.com>
2022-07-27 23:24:52 +0430

    Add governor contracts

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.9;
 
 import "./TodoListStorage.sol";
 
-library TodoLitLib {
+library TodoListLib {
     function getItem(address _storageAddress, uint256 _itemId)
         public
         view
@@ -11,6 +11,36 @@ library TodoLitLib {
     {
         return TodoListStorage(_storageAddress).getItem(_itemId);
     }
+
+    function createItem(
+        address _storageAddress,
+        string memory _title,
+        string memory _description
+    ) public {
+        return
+            TodoListStorage(_storageAddress).createItem(_title, _description);
+    }
 }
 
-contract TodoList {}
+contract TodoList {
+    using TodoListLib for address;
+    address private immutable todoListStorage;
+
+    constructor(address _todoListStorage) {
+        todoListStorage = _todoListStorage;
+    }
+
+    function createItem(string memory _title, string memory _description)
+        external
+    {
+        todoListStorage.createItem(_title, _description);
+    }
+
+    function getItem(uint256 _itemId)
+        external
+        view
+        returns (SharedStructs.TodoItem memory)
+    {
+        return todoListStorage.getItem(_itemId);
+    }
+}
